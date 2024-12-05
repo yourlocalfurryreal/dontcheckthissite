761d7bd19da532469a81a8b46dece93a150b30bd
yourlocalfurryreal <maxwellwluo@gmail.com>
2024-12-05 09:56:50 -0500

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
-var sitename = "native."; // Change this to change the name of your website.
-var subtext = "v1.2"; // set the subtext
+var sitename = "Furry's game site"; // Change this to change the name of your website.
+var subtext = "v1"; // set the subtext
 
 // more settings in main.css
 
